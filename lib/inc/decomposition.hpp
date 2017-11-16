7a51097660ac41c7ba29039e4b4bc26b393aa276
Adrian Schneider <adrian.schneider@aot.swiss>
2017-11-16 17:17:18 +0100

    Prepare LU decomposition

---
+++ lib/inc/decomposition.hpp
@@ -0,0 +1,22 @@
+#ifndef MY_DECOMPOSITION_H
+#define MY_DECOMPOSITION_H
+
+#include "matrix.hpp"
+
+class Decomposition
+{
+public:
+
+    template <class T>
+    static std::pair<Matrix<double>, Matrix<double>> luDecomposition(const Matrix<T> mat);
+};
+
+
+template <class T>
+std::pair<Matrix<double>, Matrix<double>> Decomposition::luDecomposition(const Matrix<T> mat)
+{
+
+}
+
+#endif //MY_DECOMPOSITION_H
+
---
+++ lib/test/t_decomposition.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "matrix.hpp"
+#include "decomposition.hpp"
+
+TEST(Decomposition, LU)
+{
+    ASSERT_TRUE(true);
+}
