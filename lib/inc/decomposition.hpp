14a0d26b99b304aaaa9e5831be96b1ddf11c8e36
Adrian Schneider <adrian.schneider@aot.swiss>
2017-12-04 23:31:29 +0100

    Add test case for LU-factorization

--- lib/inc/decomposition.hpp
+++ lib/inc/decomposition.hpp
@@ -7,15 +7,74 @@ class Decomposition
 {
 public:
 
+    struct LUResult
+    {
+        LUResult(Matrix<double> l, Matrix<double> u) :
+                L(l), U(u) {}
+        const Matrix<double> L;
+        const Matrix<double> U;
+    };
+
+public:
+
     template <class T>
-    static std::pair<Matrix<double>, Matrix<double>> luDecomposition(const Matrix<T>& mat);
+    static LUResult luDecomposition(const Matrix<T>& mat);
 };
 
 
 template <class T>
-std::pair<Matrix<double>, Matrix<double>> Decomposition::luDecomposition(const Matrix<T>& mat)
+Decomposition::LUResult Decomposition::luDecomposition(const Matrix<T>& mat)
 {
+    if( mat.rows() != mat.cols() )
+    {
+        std::cout << "Square matrix required";
+        std::exit(-1);
+    }
 
+    size_t s = mat.rows();
+
+    Matrix<double> l = Matrix<double>(s,s);
+    Matrix<double> u = Matrix<double>(s,s);
+
+    l.fill(0.0); u.fill(0.0);
+
+    u(0,0) = mat(0,0);
+    l.setToIdentity();
+
+    for( size_t m = 0; m < s; m++ )
+    {
+        for(size_t n = 0; n < s; m++ )
+        {
+            // compute l_mn
+            if( m > n )
+            {
+                double accum = 0;
+                for (size_t p = 0; p < n - 1; p++) {
+                    accum += l(m, p) * u(p, n);
+                }
+
+
+                l(m, n) = (mat(m, n) - accum) / u(n, n);
+            }
+
+
+
+            double accum = 0;
+            for( size_t p = 0; p < m-1; p++ )
+            {
+                accum += l(m,p)*u(p,n);
+            }
+
+            u(m,n) = mat(m,n) - accum;
+
+        }
+    }
+
+
+
+    LUResult ret(l,u);
+
+    return ret;
 }
 
 #endif //MY_DECOMPOSITION_H
--- lib/test/t_decomposition.cpp
+++ lib/test/t_decomposition.cpp
@@ -4,5 +4,19 @@
 
 TEST(Decomposition, LU)
 {
-    ASSERT_TRUE(true);
+    // https://en.wikipedia.org/wiki/LU_decomposition
+
+    double m_data[4] = {4.0, 3.0, 6.0, 3.0};
+    auto m = Matrix<double>(2, 2, m_data);
+
+    double soll_L_data[4] = {1.0, 0.0, 1.5, 1.0};
+    auto soll_L = Matrix<double>(2, 2, soll_L_data);
+
+    double soll_U_data[4] = {4.0, 3.0, 0.0, -1.5};
+    auto soll_U = Matrix<double>(2, 2, soll_U_data);
+
+    Decomposition::LUResult res = Decomposition::luDecomposition(m);
+
+    ASSERT_TRUE(soll_L.compare(res.L, 0.000001));
+    ASSERT_TRUE(soll_U.compare(res.U, 0.000001));
 }
